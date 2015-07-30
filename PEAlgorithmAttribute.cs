32f5c093636ea8351bf76233846cbec42e70891f
logdyanmicslab <lew@biba.uni-bremen.de>
2015-07-30 12:08:21 +0200

    New Attribute PEAlgorithm added.

---
+++ PEAlgorithmAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProcessingEngine.Algorithm
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	public class PEAlgorithmAttribute : Attribute
+	{
+		public PEAlgorithmAttribute(string name)
+		{
+			Name = name;
+		}
+
+        public PEAlgorithmAttribute(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        protected PEAlgorithmAttribute()
+		{
+
+		}
+
+		public string Name;
+
+        public string Description;
+
+	}
+
+}
